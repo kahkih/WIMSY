866151752bd2510b8268461be69098fbcd412dce
Cake <38938390+kahkih@users.noreply.github.com>
2019-07-25 15:13:59 +0200

    Add files via upload
    Mavenized project!

---
+++ src/main/java/io/wimsy/projectwimsy/ProjectwimsyApplication.java
@@ -0,0 +1,13 @@
+package io.wimsy.projectwimsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectwimsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectwimsyApplication.class, args);
+	}
+
+}
